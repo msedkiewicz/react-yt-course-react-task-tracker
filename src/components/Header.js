94a200689c6eca92134276c497d76b6d29ff9be9
Lena <lena@onely.com>
2022-06-06 14:23:52 +0200

    Next steps

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,10 @@
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('Click')
-  }
-
-  return (
+const Header = ({ title, onAdd, showAdd }) => {
+   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   )
 }
